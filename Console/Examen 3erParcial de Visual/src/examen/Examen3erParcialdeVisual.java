bd060c70d41149de25b7c76290794e4b275ccf21
Ulises Rocha <ulises2811.ulises@gmail.com>
2019-07-31 17:28:48 -0500

    Actividad de Servidor-Usuario

--- Console/Examen 3erParcial de Visual/src/examen/Examen3erParcialdeVisual.java
+++ Console/Examen 3erParcial de Visual/src/examen/Examen3erParcialdeVisual.java
@@ -1,6 +1,7 @@
 package examen;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  * @author Ulises
@@ -8,29 +9,45 @@ import java.util.Scanner;
 public class Examen3erParcialdeVisual {
 public static Scanner escan = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.print("Ingrese la cantidad de dinero:");
-        int cant=escan.nextInt();
+        //System.out.print("Ingrese la cantidad de dinero: ");
+        //int cant=escan.nextInt();
+        int cant=Integer.parseInt(JOptionPane.showInputDialog(""
+                + "Ingresa la cantidad Disponible de dinero: "));
         
-        System.out.print("ingrese cantidad de precios:");
-        int precios=escan.nextInt();
-        
+        //System.out.print("Ingrese cantidad de precios: ");
+        //int precios=escan.nextInt();
+        int precios=Integer.parseInt(JOptionPane.showInputDialog(""
+                + "Ingresa la cantidad de Articulos posibles a una compra con "
+                +cant+" pesos"));
         
         int[] numeros = new int[precios];
+        
         for(int x=0;x<numeros.length;x++){
-            System.out.print("ingrese valor en la posicion "+(x+1)+ ": " );
-            numeros[x]=escan.nextInt();
+            //System.out.print("ingrese valor en la posicion "+(x+1)+ ": " );
+            //numeros[x]=escan.nextInt();
+            
+            numeros[x]=Integer.parseInt(JOptionPane.showInputDialog(""
+                    + "Ingrese precio del articulo numero "+(x+1)+" : "));
         }
         
         
-        int temp=0,aux=1;
+        int temp=0,aux=0;
         for(int x=0;x<numeros.length;x++){
             temp = cant-numeros[x];
-            if((temp<cant) &&(temp>0) ){
+            if((temp<cant) && (temp>0) ){
                 aux++;
             }
         }
         
-        System.out.print("resultado: "+(aux-1));
+        for(int x=0;x<numeros.length;x++){
+            System.out.print(" "+numeros[x]);//imprime valores ingresados
+        }
+        
+        //System.out.print("resultado: "+(aux-1));
+        JOptionPane.showMessageDialog(null,""+
+                "La cantidad de articulos que puedes comprar"
+                + " con "+cant+" pesos es: "+aux);
+        
         
 }
     
